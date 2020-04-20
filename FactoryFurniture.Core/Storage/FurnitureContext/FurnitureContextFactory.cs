86876d8cad1c1e6c77007e4331e79f196b274ce3
Глеб Безруков <bezrukov96@outlook.com>
2020-04-20 05:25:26 +0300

    Правки в фабрике контекста данных

--- FactoryFurniture.Core/Storage/FurnitureContext/FurnitureContextFactory.cs
+++ FactoryFurniture.Core/Storage/FurnitureContext/FurnitureContextFactory.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Design;
 namespace FactoryFurniture.Core.Storage
 {
     /// <summary>
-    /// Реализацтя интерфейса IDesignTimeDbContextFactory для иниицализации миграции 
+    /// Реализация интерфейса IDesignTimeDbContextFactory для иниицализации миграции 
     /// </summary>
     public class FurnitureContextFactory : IDesignTimeDbContextFactory<FurnitureContext>
     {
@@ -20,22 +20,11 @@ namespace FactoryFurniture.Core.Storage
         }
         public FurnitureContext CreateDbContext(string[] args)
         {
-            string connString;
+            string connString = _connectionString;
 
-            if (!string.IsNullOrEmpty(_connectionString))
-            {
-                connString = _connectionString;
-            }
-
-            else if (args.Length > 0 && !string.IsNullOrEmpty(args[0]))
+            if (args.Length > 0 && !string.IsNullOrEmpty(args[0]))
             {
                 connString = args[0];
-            }
-
-            else
-            {
-                connString = "Integrated Security=SSPI;Pooling=false;Data Source=DESKTOP-AMECNM0\\SQLEXPRESS;Initial Catalog=FurnitureFactory";
-                // дефолтная строка подключения для инициализации миграции в Core
             }
 
             var builder = new DbContextOptionsBuilder<FurnitureContext>();
