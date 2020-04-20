1d95026e7188fce93ab6e9bd7e5a7bd8c2d99bc6
Глеб Безруков <bezrukov96@outlook.com>
2020-04-20 05:19:32 +0300

    Добавление нового проекта Core и контекста данных с фабрикой создания

---
+++ FactoryFurniture.Core/Storage/FurnitureContext/FurnitureContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using FactoryFurniture.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FactoryFurniture.Core.Storage
+{
+    /// <summary>
+    /// Контекст данных мебели
+    /// </summary>
+    public class FurnitureContext : DbContext
+    {
+        public FurnitureContext() { }
+        public FurnitureContext(DbContextOptions<FurnitureContext> options) : base(options) { }
+
+        /// <summary>
+        /// Мебель
+        /// </summary>
+        public DbSet<Furniture> Furniture { get; set; }
+
+
+        public void MigrateDb(string migrationName = null)
+        {
+            var migrate = Database.GetInfrastructure().GetRequiredService<IMigrator>();
+            if (!AllowMigrate()) return;
+            switch (string.IsNullOrEmpty(migrationName))
+            {
+                case true:
+                    migrate.Migrate();
+                    break;
+                case false:
+                    migrate.Migrate(migrationName);
+                    break;
+            }
+        }
+
+        private bool AllowMigrate()
+        {
+            return Database.GetMigrations().Count() != Database.GetAppliedMigrations().Count();
+        }
+    }
+}
---
+++ FactoryFurniture.Core/Storage/FurnitureContext/FurnitureContextFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FactoryFurniture.Core.Storage
+{
+    /// <summary>
+    /// Реализацтя интерфейса IDesignTimeDbContextFactory для иниицализации миграции 
+    /// </summary>
+    public class FurnitureContextFactory : IDesignTimeDbContextFactory<FurnitureContext>
+    {
+        private readonly string _connectionString;
+
+        public FurnitureContextFactory()
+        {
+        }
+
+        public FurnitureContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public FurnitureContext CreateDbContext(string[] args)
+        {
+            string connString;
+
+            if (!string.IsNullOrEmpty(_connectionString))
+            {
+                connString = _connectionString;
+            }
+
+            else if (!string.IsNullOrEmpty(args[0]))
+            {
+                connString = args[0];
+            }
+
+            else
+            {
+                connString = "Integrated Security=SSPI;Pooling=false;Data Source=DESKTOP-AMECNM0\\SQLEXPRESS;Initial Catalog=FurnitureFactory";
+                // дефолтная строка подключения для инициализации миграции в Core
+            }
+
+            var builder = new DbContextOptionsBuilder<FurnitureContext>();
+            builder.UseSqlServer(connString);
+            return new FurnitureContext(builder.Options);
+        }
+    }
+}
