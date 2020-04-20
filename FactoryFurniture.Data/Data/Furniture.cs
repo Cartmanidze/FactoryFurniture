4d338a20ec560b118eb5d324111d1fe5a02e9394
Глеб Безруков <bezrukov96@outlook.com>
2020-04-20 05:13:36 +0300

    Создание проекта Data и добавление POCO-объектов

---
+++ FactoryFurniture.Data/Data/Furniture.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryFurniture.Data
+{
+    /// <summary>
+    /// Реализация объекта мебели
+    /// </summary>
+    public class Furniture : BaseObject, IFurniture
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+}
